6c9cd43daa73a124def50cdb3a6898090874cf9a
Vidwan Gowda H M <107876507+gowdavidwan2003@users.noreply.github.com>
2023-10-08 20:58:32 +0530

    Update Iris_T1.py

--- Iris_T1.py
+++ Iris_T1.py
@@ -32,3 +32,23 @@ from sklearn.metrics import accuracy_score
 print("The accuracy of the model = ",accuracy_score(y_test,y_test_1)*100,'%')
 
 
+#fill your test cases here 
+# Test Case 1 - Iris Setosa
+test_case_1 = [5.0, 3.4, 1.5, 0.2]
+
+# Test Case 2 - Iris Versicolor
+test_case_2 = [6.1, 2.8, 4.7, 1.2]
+
+# Test Case 3 - Iris Virginica
+test_case_3 = [7.9, 3.8, 6.4, 2.0]
+
+
+
+prediction1 = lr.predict([test_case_1])
+print("Test case 1 prediction = ",prediction1)
+
+prediction2 = lr.predict([test_case_2])
+print("Test case 2 prediction = ",prediction2)
+
+prediction3 = lr.predict([test_case_3])
+print("Test case 3 prediction = ",prediction3)
